767903b5a37569b5ec0d1d626d708759688e2122
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-11 20:30:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,6 +36,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board,index) == false
     turn(board)
+  else puts "Please enter your player"
   end
   character = gets.strip
   move(board,index,character)

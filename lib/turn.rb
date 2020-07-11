cd9c8c60f7c6ed2c177a2812b63c517c2b7825ff
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-11 20:53:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,9 +37,9 @@ def turn(board)
   if valid_move?(board,index) == false
     turn(board)
   end
-    character = gets.strip
-    move(board,index,character)
-    display_board(board)
+  character = gets.strip
+  move(board,index,character)
+  display_board(board)
 end
 
 #def turn(input)
